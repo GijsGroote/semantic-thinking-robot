cc804277846add6f4aa1f59dee849e7cf0ccf149
Gijs Groote <gijsgroote@hotmail.com>
2021-12-07 09:41:38 +0100

    skeleton is setup

--- point_robot_example.py
+++ point_robot_example.py
@@ -1,30 +1,29 @@
 import gym
 import gijsRobot
-from gym_envs_urdf.sensors.lidar import Lidar
+# from gym_envs_urdf.sensors.lidar import Lidar
 from robot_brain import RBrain
-
+from robot_brain.main_brain import main_b
+from multiprocessing import Process, Pipe
 import numpy as np
 
 
-def main():
+def main(child_conn):
+
     # initializing gijsRobot environment
-    env = gym.make('gijsRobot-acc-v0', dt=0.05, render=True)
+    env = gym.make('gijsRobot-acc-v0', dt=0.01, render=True)
     # # adding a lidar sensor
     # lidar = Lidar(4, nbRays=4)
     # env.addSensor(lidar)
 
 
     defaultAction = np.array([0.0, 0.0])
-    n_episodes = 1
+    n_episodes = 1  # semantic brain should not need multiple episodes
     n_steps = 100000
-    cumReward = 0.0
+    cumReward = 0.0 # semantic brain does not need a reward
 
     # initial position robot
     pos0 = np.array([1.0, 0.1])
-    vel0 = np.array([1.0, 0.0])
-
-    # todo: decide if the robot brain should have it's own loop or not
-    rb = RBrain()
+    vel0 = np.array([0.0, 0.0])
 
     # reset loop
     for e in range(n_episodes):
@@ -35,11 +34,29 @@ def main():
         # main loop
         for i in range(n_steps):
             t += env.dt()
-            action = defaultAction
+            # receive data from parent connection
+            data = child_conn.recv()
+
+            action = np.array([data["x"], data["y"]])
+            # action = defaultAction
             ob, reward, done, info = env.step(action)
+
+            # send information to parent process
+            child_conn.send({"robot_accepts_input": True, "ob": ob})
             # print(done)
-            cumReward += reward
+
+            # cumReward += reward
 
 
 if __name__ == '__main__':
-    main()
+    # setup multi threading with a pipe connection
+    parent_conn, child_conn = Pipe()
+    p = Process(target=main, args=(child_conn, ))
+    # start child process
+    p.start()
+
+    # create robot brain
+    main_b(p, parent_conn)
+
+
+
--- robot_brain/main_brain.py
+++ robot_brain/main_brain.py
@@ -1,46 +1,88 @@
 from robot_brain.RBrain import RBrain
 
+from pynput import keyboard
+from pynput.keyboard import Key, Listener
+
+
+def on_press_outer(parent_conn):
+
+    def on_press(key):
+        # send the pressed key as input
+        print("key pressed: {}".format(key))
+        if key == 'w' or key == Key.up:
+            parent_conn.send({"x": 1.0, "y": 0.0})
+        if key == 's' or key == Key.down:
+            parent_conn.send({"x": -1.0, "y": 0.0})
+        if key == 'a' or key == Key.left:
+            parent_conn.send({"x": 0.0, "y": 1.0})
+        if key == 'd' or key == Key.right:
+            parent_conn.send({"x": 0.0, "y": -1.0})
+
+    return on_press
+
+
+
+def on_release_outer(parent_conn):
+    def on_press(key):
+        # set input to 0 if key is released
+
+        print("key released: {}".format(key))
+        parent_conn.send({"x": 0.0, "y": 0.0})
+
+    return on_press
+
+
 
 def main_b(p, parent_conn):
     # initialize robot brain
     brain = RBrain(parent_conn)
 
     # receive static information of the world
-    stat_world_info = parent_conn.recv()
+    # stat_world_info = parent_conn.recv()
+    stat_world_info = {"temp": "info"}
 
     # setup brain parameters of current world
     brain.setup(stat_world_info)
 
     # TODO: set a assignment/task for the robot
+    listener = keyboard.Listener(
+        on_press=on_press_outer(parent_conn),
+        on_release=on_release_outer(parent_conn))
+    listener.start()
+
+
 
     while p.is_alive():
+
         # keep on receiving information
+        parent_conn.send({"x": 0.0, "y": 0.0})
+
         data = parent_conn.recv()
 
-        if data["kill_world_process"]:
-            p.kill()
-            break
+        # if data["kill_world_process"]:
+        #     p.kill()
+        #     break
 
-        # TODO: potentially already calculate a plan before it is accepted by the robot
-        if data["robot_accepts_input"] and brain.is_doing == "executing":
-            # send input to the robot
-            brain.send_input()
-
-        elif data["robot_accepts_input"] and brain.is_doing == "nothing":
-            # For the goal set, calculate a plan
-            brain.calculate_plan()
-
-        elif data["robot_accepts_input"] and brain.is_doing == "thinking":
-            # update world that thinking of a plan is not yet done
-            # todo: if the robot is thinking it will not be able to send this message
-            parent_conn.send({"RBState": brain.is_doing, "x": 0, "y": 0})
-        elif data["robot_accepts_input"]:
-            print("input error, undefined Brain State")
-            # send zero input
-            parent_conn.send({"x": 0, "y": 0})
-        else:
-            # fallback uption
-            print("the robot is not yet ready to receive input")
+        # # TODO: potentially already calculate a plan before it is accepted by the robot
+        # if data["robot_accepts_input"] and brain.is_doing == "executing":
+        #     # send input to the robot
+        #     brain.send_input()
+        #
+        # elif data["robot_accepts_input"] and brain.is_doing == "nothing":
+        #     # For the goal set, calculate a plan
+        #     brain.calculate_plan()
+        #
+        # elif data["robot_accepts_input"] and brain.is_doing == "thinking":
+        #     # update world that thinking of a plan is not yet done
+        #     # todo: if the robot is thinking it will not be able to send this message
+        #     parent_conn.send({"RBState": brain.is_doing, "x": 0, "y": 0})
+        # elif data["robot_accepts_input"]:
+        #     print("input error, undefined Brain State")
+        #     # send zero input
+        #     parent_conn.send({"x": 0, "y": 0})
+        # else:
+        #     # fallback uption
+        #     print("the robot is not yet ready to receive input")
 
 
     # update user that this process terminates
