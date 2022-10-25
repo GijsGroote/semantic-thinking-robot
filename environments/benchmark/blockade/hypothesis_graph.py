a39da32378d4f099f3449203880d2e7e27d32a79
Gijs Groote <gijsgroote@hotmail.com>
2022-10-25 09:26:15 +0200

    small change in benchmark test

--- environments/benchmark/blockade/hypothesis_graph.py
+++ environments/benchmark/blockade/hypothesis_graph.py
@@ -7,23 +7,23 @@ def main(stage):
 
     hgraph = BoxerRobotHGraph()
 
-    hgraph.addStartNode(ObstacleSetNode(1, "start robot", []))
+    hgraph.addStartNode(ObstacleNode(1, "start robot", []))
     hgraph.addTargetNode(StateNode(3, "target cube", []))
-    hgraph.addStartNode(ObstacleSetNode(2, "R: start cube", []))
+    hgraph.addStartNode(ObstacleNode(2, "R: start cube", []))
 
     if stage==1:
 
-        node1 = ObstacleSetNode(1, "start robot", [])
+        node1 = ObstacleNode(1, "start robot", [])
         hgraph.current_node = node1
         hgraph.addStartNode(node1)
-        hgraph.addNode(ObstacleSetNode(4, "RM: cube", []))
+        hgraph.addNode(ObstacleNode(4, "RM: cube", []))
         hgraph.addEdge(Edge("id", 1, 2, "MPC", "controller", True))
         hgraph.addEdge(Edge("id", 4, 3, "MPPI", "controller"))
         hgraph.addEdge(Edge("id", 2, 4, "Forward Model", "controller", True))
 
     if stage==2:
         
-        node4 = ObstacleSetNode(4, "RM: cube", [])
+        node4 = ObstacleNode(4, "RM: cube", [])
         hgraph.current_node = node4
         hgraph.addNode(node4)
         hgraph.addEdge(Edge("id", 1, 2, "MPC", "controller", True))
@@ -33,7 +33,7 @@ def main(stage):
 
     if stage>=3 and stage<5:
 
-        node4 = ObstacleSetNode(4, "RM: cube", [])
+        node4 = ObstacleNode(4, "RM: cube", [])
         if stage == 3:
             hgraph.current_node = node4
         hgraph.addNode(node4)
@@ -55,11 +55,11 @@ def main(stage):
         hgraph.current_node = node7
 
     if stage == 5:
-        node4 = ObstacleSetNode(4, "RM: cube", [])
+        node4 = ObstacleNode(4, "RM: cube", [])
         hgraph.addNode(node4)
         hgraph.addNode(StateNode(5, "RM: cube", []))
         hgraph.addNode(StateNode(6, "(aborted) R: target green wall ", []))
-        node7 = ObstacleSetNode(7, "RM: green wall", [])
+        node7 = ObstacleNode(7, "RM: green wall", [])
         hgraph.current_node = node7
         hgraph.addNode(node7)
         hgraph.addNode(StateNode(8, "R: green wall", []))
@@ -72,11 +72,11 @@ def main(stage):
         hgraph.addEdge(Edge("id", 7, 3, "MPPI", "controller"))
 
     if stage==6:
-        node4 = ObstacleSetNode(4, "RM: cube", [])
+        node4 = ObstacleNode(4, "RM: cube", [])
         hgraph.addNode(node4)
         hgraph.addNode(StateNode(5, "RM: cube", []))
         hgraph.addNode(StateNode(6, "(aborted) R: target green wall", []))
-        node7 = ObstacleSetNode(7, "RM: green wall", [])
+        node7 = ObstacleNode(7, "RM: green wall", [])
         hgraph.current_node = node7
         hgraph.addNode(node7)
         hgraph.addNode(StateNode(8, "R: green wall", []))
@@ -97,11 +97,11 @@ def main(stage):
         hgraph.addEdge(Edge("id", 14, 3, "EMPPI", "controller"))
  
     if stage==7:
-        node4 = ObstacleSetNode(4, "RM: cube", [])
+        node4 = ObstacleNode(4, "RM: cube", [])
         hgraph.addNode(node4)
         hgraph.addNode(StateNode(5, "RM: cube", []))
         hgraph.addNode(StateNode(6, "(aborted) R: target green wall", []))
-        node7 = ObstacleSetNode(7, "RM: green wall", [])
+        node7 = ObstacleNode(7, "RM: green wall", [])
         hgraph.addNode(node7)
         hgraph.addNode(StateNode(8, "R: green wall", []))
         hgraph.addEdge(Edge("id", 1, 2, "MPC", "controller", True))
